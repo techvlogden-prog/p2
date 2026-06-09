507596f02fe007883d3ff7853a0e9a7c0b3feb0e
Your Name <your@example.com>
2026-06-09 09:35:02 +0530

    Portfolio UI enhancement

--- packages/js/main.js
+++ packages/js/main.js
@@ -206,20 +206,3 @@ if (themeButton) {
     );
   });
 }
-const track = document.querySelector('.certificate-track');
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
-
-nextBtn.addEventListener('click', () => {
-track.scrollBy({
-left: 380,
-behavior: 'smooth'
-});
-});
-
-prevBtn.addEventListener('click', () => {
-track.scrollBy({
-left: -380,
-behavior: 'smooth'
-});
-});
